14093daa40a4d256bd06f41dd5347187347c0e07
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2010-08-14 11:45:24 +0200

    init

---
+++ src/main/clojure/kata/Main.clj
@@ -0,0 +1,10 @@
+(ns ^{:author "B. P. Smith-Mannschott, bsmith.occs@gmail.com"
+      :doc "Code kata main class."}
+  kata.Main	
+  (:gen-class))
+
+(defn -main
+  [& args]
+  (println "Hello World!"))
+
+
